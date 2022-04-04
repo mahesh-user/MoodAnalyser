8f40e9883fe3273a1264fbef4d9459ffb2dfd21c
Mahesh <mahesh51138@gmail.com>
2022-04-05 01:18:33 +0530

    rectored:Analyzation Of Mood

--- src/com/bridgelabz/MoodAnalyser.java
+++ src/com/bridgelabz/MoodAnalyser.java
@@ -1,9 +1,14 @@
 package com.bridgelabz;
+//purpose:This class is to analyse the mood of the user
 
+//auther:mahesh
 public class MoodAnalyser {
 
     public String analyseMood(String str) {
-
+        /**
+         * this method is to analyse the mood
+         * @param str
+         */
         if (str.contains("Sad")){
             return "Sad";
         }
