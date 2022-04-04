4503a1e8bcdc65091d2cd0688d9101b9e52019f0
Mahesh <mahesh51138@gmail.com>
2022-04-05 01:13:38 +0530

    refactored:Null Pointer Expetion Handling

--- src/com/bridgelabz/MoodAnalyser.java
+++ src/com/bridgelabz/MoodAnalyser.java
@@ -1,4 +1,7 @@
 package com.bridgelabz;
+//purpose:This class is to analyse the mood of the user by using expetions
+
+//auther:mahesh
 
 public class MoodAnalyser {
 
@@ -6,16 +9,26 @@ public class MoodAnalyser {
 
 
 
+    /**
+     * Default constructor
+     */
     public MoodAnalyser(){
 
     }
-
+    /**Parameterized constructor to assign value for the string message
+     *
+     * @param message
+     */
 
 
     public MoodAnalyser(String message){
         this.message = message;
     }
 
+    /**
+     * this method is to analyse the mood by using expetions
+     * @return sad or happy
+     */
 
 
     public String analyseMood() {
